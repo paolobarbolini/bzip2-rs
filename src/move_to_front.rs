633a57cd27711e3f291a635d3cb66b713b07257b
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-03-04 21:34:14 +0530

    Added large and small repeating tests for PositionalMTFEncoder
    And all of them pass! :D

--- src/move_to_front.rs
+++ src/move_to_front.rs
@@ -208,9 +208,7 @@ mod encoder_tests {
         count == a.len() && count == b.len()
     }
 
-    #[test]
-    fn encode_distinct_bytes() {
-        let buf = b"abcd1234";
+    fn check_mtfe(buf: &[u8], answer: &[u16], mtf_freq: &[usize]) {
         let nblock = buf.len();
         let ptr: Vec<usize> = (1..=nblock).collect();
         let mut in_use = [false; PositionalMTFEncoder::BYTE_RANGE];
@@ -222,11 +220,32 @@ mod encoder_tests {
         let mut mtfe = PositionalMTFEncoder::new();
         mtfe.encode(nblock, buf, &ptr, &in_use);
 
-        let answer = [5, 6, 7, 8, 5, 6, 7, 8, 9];
-        let mtf_freq = [0, 0, 0, 0, 0, 2, 2, 2, 2, 1];
-
-        println!("{:?} {:?}", mtfe.selectors, mtfe.mtf_freq);
         assert!(vector_equals(&answer, &mtfe.selectors));
         assert!(vector_equals(&mtf_freq, &mtfe.mtf_freq));
+    }
+
+    #[test]
+    fn encode_distinct_bytes() {
+        let buf = b"abcd1234";
+        let answer = [5, 6, 7, 8, 5, 6, 7, 8, 9];
+        let mtf_freq = [0, 0, 0, 0, 0, 2, 2, 2, 2, 1];
+        check_mtfe(buf, &answer, &mtf_freq);
+    }
+
+    #[test]
+    fn encode_repeating_small() {
+        let buf = b"ab1ccb1bcddd2aa34";
+        let answer = [5, 6, 3, 7, 0, 3, 3, 2, 3, 8, 1, 6, 6, 0, 7, 8, 9];
+        let mtf_freq = [2, 1, 1, 4, 0, 1, 3, 2, 2, 1];
+
+        check_mtfe(buf, &answer, &mtf_freq);
+    }
+
+    #[test]
+    fn encode_repeating_large() {
+        let buf = b"972938M0o1Dwy5T4afiCDM1sJ227jot92F35cJwpivOOK13yvPOEdI177zxQVk82N1SWK1e962xPngky02445blwn9mEz3T3LCwW";
+        let answer = [10, 9, 5, 3, 6, 10, 19, 7, 41, 9, 14, 46, 48, 14, 28, 15, 31, 36, 38, 20, 10, 14, 12, 45, 25, 20, 0, 21, 40, 18, 46, 23, 6, 26, 24, 20, 38, 11, 23, 46, 19, 47, 34, 0, 32, 19, 11, 27, 6, 35, 7, 32, 41, 34, 9, 23, 0, 49, 49, 39, 41, 46, 38, 25, 41, 10, 42, 43, 21, 4, 45, 30, 43, 9, 14, 21, 49, 47, 15, 25, 45, 8, 44, 0, 34, 47, 48, 34, 11, 15, 49, 30, 27, 33, 48, 2, 49, 47, 10, 26, 50];
+        let mtf_freq = [4, 0, 1, 1, 1, 1, 3, 2, 1, 4, 5, 3, 1, 0, 4, 3, 0, 0, 1, 3, 3, 3, 0, 3, 1, 3, 2, 2, 1, 0, 2, 1, 2, 1, 4, 1, 1, 0, 3, 1, 1, 4, 1, 2, 1, 3, 4, 4, 3, 5, 1];
+        check_mtfe(buf, &answer, &mtf_freq);
     }
 }
